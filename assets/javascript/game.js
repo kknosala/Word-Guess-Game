dd8dfb53517f4c8843021d549645e9067317a8ee
KSK <kurtknosala@gmail.com>
2019-07-07 13:41:17 -0600

    reset logic complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,8 @@
-var wordChoice = ['angola', 'germany', 'micronesia', 'philippines', 'uzbekistan'];
-var playerLetters = "";
+// var wordChoice = [['a','n','g','o','l','a'], ['g','e','r','m','a','n','y'], ['m','i','c','o','n','e','s','i','a'], ['p','i','l','i','p','p','i','n','e','s'], ['u','z','b','e','k','i','s','t','a','n']];
+//Word Choices angola, germany, micronesia, pilippines, uzbekistan
+
+var wordChoice = ['angola', 'germany', 'micronesia', 'philippines', 'uzbekistan']
+var wordChoiceText = "";
 var guessesLeft = 10;
 
 var playerGuessesText = document.getElementById('guesses-made');
@@ -7,6 +10,31 @@ var wordChosenText = document.getElementById('word-to-guess');
 var guessesLeftText = document.getElementById('guesses-left');
 
 guessesLeftText.textContent = guessesLeft;
+
+function wordGenerator(){
+
+    var gameWord = wordChoice[Math.floor(Math.random() * 5)];
+
+    for(var i = 0; i < gameWord.length; i++){
+        wordChoiceText = gameWord[i];
+        wordChosenText.textContent += wordChoiceText;
+    }
+
+}
+
+function resetGame(){
+    
+    wordChoiceText = '';
+    wordChosenText.textContent = wordChoiceText;
+    wordGenerator();
+    playerGuess = '';
+    playerGuessesText.textContent = playerGuess;
+    guessesLeft = 10;
+    guessesLeftText.textcontent = guessesLeft;
+}
+
+wordGenerator();
+
 
 document.onkeyup = function(event) {
 
@@ -17,4 +45,10 @@ document.onkeyup = function(event) {
     guessesLeftText.textContent = guessesLeft;
 
     playerGuessesText.textContent += playerGuess + ", "
+
+    
+    if(guessesLeft === 0){
+        resetGame();
+    }
+
 }
